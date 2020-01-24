839025cf7ed357362b27c4423d92c8ce40c5755f
linsalrob <raedwards@gmail.com>
2020-01-23 16:09:08 -0800

    adding json output

--- pppf_lib/cluster_summary.py
+++ pppf_lib/cluster_summary.py
@@ -8,11 +8,13 @@ import os
 import sys
 import argparse
 import pickle
+import json
 
 from roblib import bcolors
 from cluster import Cluster
 from formatting import color
 from database_handles import connect_to_db
+from . import is_hypothetical
 
 def read_mmseqs_clusters(clf, verbose=False):
     """
@@ -49,6 +51,23 @@ def read_mmseqs_clusters(clf, verbose=False):
     if verbose:
         sys.stderr.write(f"{color.BLUE}There were {cc} clusters{color.ENDC}\n")
     return cls
+
+def cluster_is_hypothetical(cl, verbose=False):
+    """
+    Check the functions of the members of a cluster and determine whether they are
+    all hypothetical
+    :param cl: the cluster with enriched data
+    :param verbose: more output
+    :return: True if all hypothetical. False if not
+    """
+
+    for f in cl.functions:
+        if not is_hypothetical(f):
+            return False
+
+    return True
+
+
 
 def enrich_a_cluster(clid, mems, cur, exout=None, verbose=False):
     """
@@ -126,6 +145,7 @@ def enrich_cluster_data(cls, dbf, summf, exout=None, verbose=False):
     for cl in cls:
         cl.shortest_id, cl.shortest_len, cl.longest_id, cl.longest_len, cl.functions = enrich_a_cluster(cl.id, list(cl.members), cur, exout, verbose)
         cl.number_of_functions = len(cl.functions)
+        cl.only_hypothetical = cluster_is_hypothetical(cl, verbose)
         out.write(f"{cl.exemplar}\t{cl.number_of_members}\t{cl.number_of_functions}\t{cl.shortest_len}\t{cl.longest_len}\n")
         updatedcls.append(cl)
 
@@ -148,6 +168,23 @@ def pickle_cluster_data(cl, pf, verbose=False):
     with open(pf, 'wb') as po:
         pickle.dump(cl, po)
 
+
+def json_cluster_data(cl, jf, verbose=False):
+    """
+    Write the cluster data in json format (not recommended for large data sets!)
+    :param cl: The clusters
+    :param jf: The json file to write
+    :param verbose: more output
+    :return:
+    """
+
+
+    if verbose:
+        sys.stderr.write(f"{color.GREEN}Writing json data{color.ENDC}\n")
+
+    with open(jf, 'wb') as po:
+        json.dump(cl, po)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Summarize a set of clusters from mmseqs (or elsewhere)')
     parser.add_argument('-t', help='mmseqs format tsv file of clusters')
@@ -155,6 +192,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', help='summary file to write', required=True)
     parser.add_argument('-p', help='pickle file to write with enriched cluster information')
     parser.add_argument('-e', help='enriched cluster output file')
+    parser.add_argument('-j', help='write a json file with the enriched cluster information')
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
@@ -168,6 +206,9 @@ if __name__ == '__main__':
 
     if args.p:
         pickle_cluster_data(newclusters, args.p, args.v)
+
+    if args.j:
+        json_cluster_data(newclusters, args.j, args.v)
 
 
 
