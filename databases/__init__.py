3292f9b7043de9cfc30b8b4f5bfbaafacb44cda4
Robert Edwards <raedwards@gmail.com>
2020-02-04 22:49:14 -0800

    resolving relative import

--- databases/__init__.py
+++ databases/__init__.py
@@ -1,6 +1,6 @@
-from define_database_tables import define_all_tables
-from database_handles import connect_to_db, disconnect
-from load_sequences_from_genbank import load_genbank_file
+from .define_database_tables import define_all_tables
+from .database_handles import connect_to_db, disconnect
+from .load_sequences_from_genbank import load_genbank_file
 
 __all__ = [
     'define_all_tables', 'connect_to_db', 'disconnect',
