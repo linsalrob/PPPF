3784e573beebcfa52b8432bf1b6853ba0ecf1b14
linsalrob <raedwards@gmail.com>
2020-02-05 16:34:22 -0800

    redesigning protein cluster tables

--- pppf_clusters/__init__.py
+++ pppf_clusters/__init__.py
@@ -2,7 +2,7 @@
 Definitions for the pppf_cluster methods
 """
 
-from .load_clusters_to_database import read_mmseqs_clusters, add_functions_to_clusters, insert_cluster_metadata, insert_into_database,
+from .load_clusters_to_database import read_mmseqs_clusters, add_functions_to_clusters, insert_cluster_metadata, insert_into_database
 from .cluster import Cluster
 
 __all__ = [
