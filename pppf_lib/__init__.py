c4089bd5412bf790d680806280fe7c3f08bc2657
linsalrob <raedwards@gmail.com>
2020-02-11 11:34:33 -0800

    Using biopython to download sequences from genbank

--- pppf_lib/__init__.py
+++ pppf_lib/__init__.py
@@ -1,6 +1,8 @@
 from .formatting import color, colour
 from .functions import is_hypothetical
+from .genbank_download import GenBankDownload
 
 __all__ = [
-    'color', 'colour', 'is_hypothetical'
+    'color', 'colour', 'is_hypothetical',
+    'GenBankDownload'
 ]
