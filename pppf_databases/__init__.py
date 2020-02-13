1252ccb34ef7440f4e3610e3e12282060e88d07c
linsalrob <raedwards@gmail.com>
2020-02-12 16:52:36 -0800

    adding fasta

--- pppf_databases/__init__.py
+++ pppf_databases/__init__.py
@@ -1,8 +1,9 @@
 from .define_database_tables import define_all_tables
 from .database_handles import connect_to_db, disconnect
 from .load_sequences_from_genbank import load_genbank_file
+from .db_to_fasta import protein_to_fasta
 
 __all__ = [
     'define_all_tables', 'connect_to_db', 'disconnect',
-    'load_genbank_file'
+    'load_genbank_file', 'protein_to_fasta'
 ]
