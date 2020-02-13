e44765b110ebd7e8c9dc12234800fe1797a251ad
linsalrob <raedwards@gmail.com>
2020-02-12 16:53:51 -0800

    dumping fasta sequences from our database

---
+++ pppf_databases/db_to_fasta.py
@@ -0,0 +1,31 @@
+
+from pppf_lib import color
+import sys
+__author__ = 'Rob Edwards'
+
+
+def protein_to_fasta(conn, outputfile=None, verbose=False):
+    """
+    Extract the protein sequences from the SQLite database and
+    print them out in fasta format.
+
+    Note that at the moment this uses the md5sum as the ID, although
+    an option to extend this would be to use the original IDs from the
+    protein table. If you want that, please create a GitHub issue.
+
+    :param conn: the database connection
+    :param outputfile: the name of the output file. Otherwise stdout
+    :param verbose: more output
+    """
+
+    if verbose:
+        sys.stderr.write(f"{color.GREEN}Creating fasta file{color.ENDC}\n")
+
+    ex = conn.curr().execute("SELECT protein_md5sum, protein_sequence from protein_sequence")
+    out = open(outputfile, 'w') if outputfile else sys.stdout
+    for row in ex.fetchall():
+        out.write(f">{row[0]}\n{row[1]}\n")
+    
+    if out is not sys.stdout:
+        out.close() 
+        
---
+++ scripts/dump_protein_fasta.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+import os
+import sys
+import argparse
+from pppf_databases import protein_to_fasta, connect_to_db, disconnect
+
+__author__ = 'Rob Edwards'
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=' ')
+    parser.add_argument('-d', help="sqlite database", required=True)
+    parser.add_argument('-f', help='input file', required=True)
+    parser.add_argument('-v', help='verbose output', action='store_true')
+    args = parser.parse_args()
+
+    conn = connect_to_db(args.d, args.v)
+    protein_to_fasta(conn, args.f, args.v)
+    disconnect(conn, args.v)
+
+
