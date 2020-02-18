7aa0cc28a7c4601316e0d6cee6775fe1470fd69b
linsalrob <raedwards@gmail.com>
2020-02-18 10:36:59 -0800

    separating creating and loading

--- scripts/load_databases.py
+++ scripts/load_databases.py
@@ -13,11 +13,18 @@ from pppf_databases import load_genbank_file, connect_to_db, disconnect, define_
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Create a database and load it with GenBank data")
-    parser.add_argument('-d', help='SQL output database', required=True)
+    parser.add_argument('-f', help='genbank file to load', nargs="+", action='append')
+    parser.add_argument('-d', help='SQL database', required=True)
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
     conn = connect_to_db(args.d, args.v)
-    sys.stderr.write(f"{color.BOLD}{color.BLUE}Defining Tables{color.ENDC}\n")
-    define_all_tables(conn, args.v)
+    sys.stderr.write(f"{color.BOLD}{color.BLUE}Loading data{color.ENDC}\n")
+    count = 0
+    tc = len(args.f)
+    for f in args.f:
+        count+=1
+        if args.v:
+            sys.stderr.write(f"{color.GREEN}File {count} of {tc}: {f}\n{color.ENDC}")
+        load_genbank_file(f, conn, args.v)
     disconnect(conn, args.v)
