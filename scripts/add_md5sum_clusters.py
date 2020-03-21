e5aa38c55dca0f1b24ee253157c88ceef167498f
Robert Edwards <raedwards@gmail.com>
2020-03-20 17:44:58 -0700

    Creating the table too

--- scripts/add_md5sum_clusters.py
+++ scripts/add_md5sum_clusters.py
@@ -6,6 +6,7 @@ import os
 import sys
 import argparse
 from pppf_databases import connect_to_db, disconnect
+from pppf_lib import color
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
@@ -14,6 +15,26 @@ if __name__ == '__main__':
     args = parser.parse_args()
     dbc = connect_to_db(args.c, args.v)
     cur = dbc.cursor()
+
+    # define the table
+    if args.v:
+        sys.stderr.write(f"{color.GREEN}Creating MD5CLUSTERS table{color.ENDC}\n")
+
+    cur.execute("""
+        CREATE TABLE md5cluster (
+            md5cluster_rowid INTEGER PRIMARY KEY,
+            protein_md5sum TEXT NOT NULL,
+            cluster INTEGER NOT NULL,
+            FOREIGN KEY(cluster) REFERENCES cluster(cluster_rowid)
+        )""")
+    cur.execute("CREATE UNIQUE INDEX md5cluster_idx1 ON md5cluster(md5cluster_rowid);")
+    cur.execute("CREATE INDEX md5cluster_idx2 ON md5cluster(md5cluster_rowid, protein_md5sum);")
+    cur.execute("CREATE INDEX md5cluster_idx3 ON md5cluster(md5cluster_rowid, cluster);")
+    cur.execute("CREATE INDEX md5cluster_idx4 ON md5cluster(protein_md5sum, cluster);")
+    cur.execute("CREATE INDEX md5cluster_idx5 ON md5cluster(cluster, protein_md5sum);")
+    dbc.commit()
+
+
 
     # get all the clusters
     ex = cur.execute("select cluster_rowid, members from cluster")
