9e8403bd9eb5a0a7b258ceb94f9bb173212792b7
Robert Edwards <raedwards@gmail.com>
2020-03-22 15:30:47 -0700

    correcting help menu

--- scripts/dump_protein_fasta.py
+++ scripts/dump_protein_fasta.py
@@ -16,7 +16,7 @@ __author__ = 'Rob Edwards'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=' ')
     parser.add_argument('-p', help="phage sqlite database", required=True)
-    parser.add_argument('-f', help='input file', required=True)
+    parser.add_argument('-f', help='output fasta file', required=True)
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
