c90c96b613fee4b5c1e85534d24a6a5cc780a5aa
linsalrob <raedwards@gmail.com>
2020-03-31 16:42:21 -0700

    adding cluster functions

--- pppf_clusters/__init__.py
+++ pppf_clusters/__init__.py
@@ -4,8 +4,11 @@ Definitions for the pppf_cluster methods
 
 from .load_clusters_to_database import read_mmseqs_clusters, add_functions_to_clusters, insert_cluster_metadata, insert_into_database
 from .cluster import Cluster
+from .cluster_functions import proteinid_to_function, proteinid_to_all_functions
 
 __all__ = [
     'read_mmseqs_clusters', 'add_functions_to_clusters', 'insert_cluster_metadata', 'insert_into_database',
-    'Cluster'
+    'Cluster', 'proteinid_to_function', 'proteinid_to_all_functions'
 ]
+
+
