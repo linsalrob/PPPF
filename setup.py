6f4d93dda2a7f5a06b7e176acd3a4078855a314d
Robert Edwards <raedwards@gmail.com>
2020-05-04 18:58:03 -0700

    adding script to do the downloads

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PPPF-linsalrob',
-    version='0.0.1',
+    version='0.0.2',
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -13,8 +13,13 @@ setuptools.setup(
     license='MIT License',
     author='Rob Edwards',
     author_email='raedwards@gmail.com',
-    description='Phage genomes and their annotations ',
+    description='Probabilistic Phage Protein Functions: Phage genomes and their annotations ',
     python_requires='>=3.6',
+    install_requires=[
+        'jsonpickle',
+        'biopython',
+        'requests',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -23,4 +28,9 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
     ],
+    entry_points = {
+        'console_scripts' : [
+            'pppf_download_databases = pppf_databases:download_all_databases',
+        ]
+    }
 )
