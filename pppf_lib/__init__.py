d80b062b9dcd5295a77301dcea16598ee87b36b4
Robert Edwards <raedwards@gmail.com>
2020-05-04 17:25:17 -0700

    moving color to accessories

--- pppf_lib/__init__.py
+++ pppf_lib/__init__.py
@@ -1,10 +1,9 @@
-from .formatting import color, colour
 from .functions import is_hypothetical
 from .genbank import GenBank
 from .genbank_download import GenBankDownload
 from .genbank_search import GenBankSearch
 
 __all__ = [
-    'color', 'colour', 'is_hypothetical',
+    'is_hypothetical',
     'GenBank', 'GenBankDownload', 'GenBankSearch'
 ]
