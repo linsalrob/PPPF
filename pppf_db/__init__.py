041625bce1f7c84fb9e771cd5e6335b98e27416f
Robert Edwards <raedwards@gmail.com>
2020-05-04 20:15:23 -0700

    adding genome methods

--- pppf_db/__init__.py
+++ pppf_db/__init__.py
@@ -4,11 +4,12 @@ init file for pppf_db
 
 from .db_paths import phagedb, clustersdb
 from .database_handles import connect_to_db, disconnect
-from .protein import lookup_word
-from .dump_protein_fasta import print_all_proteins
-
+from .proteins import lookup_word, print_all_proteins
+from .genomes import list_all_genomes
 
 __all__ = [
-    'phagedb', 'clustersdb', 'connect_to_db', 'disconnect',
-    'print_all_proteins'
+    'phagedb', 'clustersdb',
+    'connect_to_db', 'disconnect',
+    'lookup_word', 'print_all_proteins',
+    'list_all_genomes'
 ]
