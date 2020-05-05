015813e508bf9a9899627bd3b6dd96965e714554
Robert Edwards <raedwards@gmail.com>
2020-05-04 19:07:49 -0700

    incrementing version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='PPPF-linsalrob',
-    version='0.0.3',
+    version='0.0.4',
     packages=setuptools.find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
