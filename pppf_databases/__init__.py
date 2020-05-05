141e8aa4d78115ac440988bc95c70b08265c7c13
Robert Edwards <raedwards@gmail.com>
2020-05-04 18:55:32 -0700

    Adding a new download_all_databases function

--- pppf_databases/__init__.py
+++ pppf_databases/__init__.py
@@ -2,8 +2,8 @@ from .define_database_tables import define_phage_tables, define_cluster_tables
 from .database_handles import connect_to_db, disconnect
 from .load_sequences_from_genbank import load_genbank_file
 from .db_to_fasta import protein_to_fasta
-
+from .download_databases import download_all_databases
 __all__ = [
     'define_phage_tables', 'define_cluster_tables', 'connect_to_db', 'disconnect',
-    'load_genbank_file', 'protein_to_fasta'
+    'load_genbank_file', 'protein_to_fasta', 'download_all_databases'
 ]
