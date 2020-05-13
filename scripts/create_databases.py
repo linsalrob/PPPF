75898712d30553f5802a74734f81fe53a596bcdb
linsalrob <raedwards@gmail.com>
2020-05-13 13:02:33 -0700

    creating empty files if they dont exist

--- scripts/create_databases.py
+++ scripts/create_databases.py
@@ -21,6 +21,9 @@ if __name__ == '__main__':
 
     if args.p:
         sys.stderr.write(f"{color.BOLD}{color.BLUE}Defining Phage Tables{color.ENDC}\n")
+        if not os.path.exists(args.p):
+            with open(args.p, 'w') as out:
+                True
         phageconn = connect_to_db(args.p, args.v)
         define_phage_tables(phageconn, args.v)
         phageconn.commit()  # final commit to make sure everything saved!
@@ -28,6 +31,9 @@ if __name__ == '__main__':
 
     if args.c:
         sys.stderr.write(f"{color.BOLD}{color.BLUE}Defining Cluster Tables{color.ENDC}\n")
+        if not os.path.exists(args.c):
+            with open(args.c, 'w') as out:
+                True
         clconn    = connect_to_db(args.c, args.v)
         define_cluster_tables(clconn, args.v)
         clconn.commit()
